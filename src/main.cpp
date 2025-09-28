f5e54958fe922f4e469629075d208418ae303402
auser <unnmd88@gmail.com>
2025-09-28 21:12:04 +0300

    Test flashing

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 // uint8_t newAddress = 0x09;                                          //   Назначаемый модулю адрес (0x07 < адрес < 0x7F).
                                                                     //
 #include <Wire.h>                                                   //   Подключаем библиотеку для работы с аппаратной шиной I2C, до подключения библиотеки iarduino_I2C_Relay.
+#include <avr/interrupt.h>
 #include <iarduino_I2C_Relay.h>
 #include <Arduino.h>
 
@@ -21,7 +22,9 @@
 iarduino_I2C_Relay relay;  
 Direction LEFT {0, INPUT_PULLUP, 1};
 Direction STRAIGHT {1, INPUT_PULLUP, 2};
-Direction RIGTH {2, INPUT_PULLUP, 3};
+Direction RIGHT {2, INPUT_PULLUP, 3};
+
+byte cur {0};
 
 void setup(){
   delay(500);       
@@ -32,39 +35,92 @@ void setup(){
 }
 
 void loop(){
-
-  if (STRAIGHT.pinIsActive()) STRAIGHT.setState(CurrentState::allowed);
-  if (STRAIGHT.curState == CurrentState::allowed && !STRAIGHT.pinIsActive() && STRAIGHT.timeStampChangeState == 0)
+  cur = !digitalRead(STRAIGHT.pinNum);
+  if (cur != STRAIGHT.prevPinState && STRAIGHT.curState != CurrentState::flashing) 
   {
-    STRAIGHT.setState(CurrentState::flashing);
     STRAIGHT.timeStampChangeState = millis();
+    STRAIGHT.requestChangeState = true;
+    STRAIGHT.rest = false;
+    STRAIGHT.prevPinState = cur;
   }
 
-  if (STRAIGHT.curState == CurrentState::flashing) 
-  {
-    if ((millis() - STRAIGHT.timeStampChangeState) <= 1) relay.digitalWrite(STRAIGHT.relayNum, 0);
-    else if ((millis() - STRAIGHT.timeStampChangeState) == 500) relay.digitalWrite(STRAIGHT.relayNum, 1);
-    else if ((millis() - STRAIGHT.timeStampChangeState) == 1000) relay.digitalWrite(STRAIGHT.relayNum, 0);
-    else if ((millis() - STRAIGHT.timeStampChangeState) == 1500) relay.digitalWrite(STRAIGHT.relayNum, 1);
-    else if ((millis() - STRAIGHT.timeStampChangeState) == 2000) relay.digitalWrite(STRAIGHT.relayNum, 0);
-    else if ((millis() - STRAIGHT.timeStampChangeState) == 2500) relay.digitalWrite(STRAIGHT.relayNum, 1);
-    else if ((millis() - STRAIGHT.timeStampChangeState) >= 3000) 
+  if (STRAIGHT.requestChangeState && millis() - STRAIGHT.timeStampChangeState > 50 && STRAIGHT.curState != CurrentState::flashing)
+    if (!cur)
     {
-      STRAIGHT.setState(CurrentState::forbidden);
+      STRAIGHT.curState = CurrentState::flashing;
+      STRAIGHT.timeStampChangeStateFlashing = millis();
+    }
+    else 
+    {
+      relay.digitalWrite(STRAIGHT.relayNum, cur);
+      STRAIGHT.requestChangeState = false;
       STRAIGHT.timeStampChangeState = 0;
-    };
-    
-  }
+      STRAIGHT.rest = true;
+      STRAIGHT.curState = CurrentState::allowed;
+    }
   
-  else if (STRAIGHT.curState == CurrentState::allowed && !relay.digitalRead(STRAIGHT.relayNum)) relay.digitalWrite(STRAIGHT.relayNum, 1);
+  if (STRAIGHT.curState == CurrentState::flashing)
+  {
+
+    if ((millis() - STRAIGHT.timeStampChangeStateFlashing) <= 1) relay.digitalWrite(STRAIGHT.relayNum, 0);
+    else if ((millis() - STRAIGHT.timeStampChangeStateFlashing) == 500) relay.digitalWrite(STRAIGHT.relayNum, 1);
+    else if ((millis() - STRAIGHT.timeStampChangeStateFlashing) == 1000) relay.digitalWrite(STRAIGHT.relayNum, 0);
+    else if ((millis() - STRAIGHT.timeStampChangeStateFlashing) == 1500) relay.digitalWrite(STRAIGHT.relayNum, 1);
+    else if ((millis() - STRAIGHT.timeStampChangeStateFlashing) == 2000) relay.digitalWrite(STRAIGHT.relayNum, 0);
+    else if ((millis() - STRAIGHT.timeStampChangeStateFlashing) == 2500) relay.digitalWrite(STRAIGHT.relayNum, 1);
+    else if ((millis() - STRAIGHT.timeStampChangeStateFlashing) == 3000) 
+    {
+      relay.digitalWrite(STRAIGHT.relayNum, 0);
+      STRAIGHT.requestChangeState = false;
+      STRAIGHT.rest = true;
+      STRAIGHT.prevPinState = 0;
+      STRAIGHT.curState = CurrentState::forbidden;
+    }
+  }
+
+  return;
 
 
 
-  // if (LEFT.curState == CurrentState::forbidden && STRAIGHT.curState == CurrentState::forbidden && RIGTH.curState == CurrentState::forbidden) 
+
+  
+
+
+
+
+
+  // if (STRAIGHT.pinIsActive()) STRAIGHT.setState(CurrentState::allowed);
+  // if (STRAIGHT.curState == CurrentState::allowed && !STRAIGHT.pinIsActive() && STRAIGHT.timeStampChangeState == 0)
+  // {
+  //   STRAIGHT.setState(CurrentState::flashing);
+  //   STRAIGHT.timeStampChangeState = millis();
+  // }
+  // if ((STRAIGHT.curState == CurrentState::flashing) && (millis() - STRAIGHT.timeStampChangeState) >= 3010) 
+  // {
+  //   STRAIGHT.setState(CurrentState::forbidden);
+  //   STRAIGHT.timeStampChangeState = 0;
+  // }
+    
+
+  // // Секция установки выходов на реле
+  // if (STRAIGHT.curState == CurrentState::flashing) 
+  // {
+  //   if ((millis() - STRAIGHT.timeStampChangeState) <= 1) relay.digitalWrite(STRAIGHT.relayNum, 0);
+  //   else if ((millis() - STRAIGHT.timeStampChangeState) == 500) relay.digitalWrite(STRAIGHT.relayNum, 1);
+  //   else if ((millis() - STRAIGHT.timeStampChangeState) == 1000) relay.digitalWrite(STRAIGHT.relayNum, 0);
+  //   else if ((millis() - STRAIGHT.timeStampChangeState) == 1500) relay.digitalWrite(STRAIGHT.relayNum, 1);
+  //   else if ((millis() - STRAIGHT.timeStampChangeState) == 2000) relay.digitalWrite(STRAIGHT.relayNum, 0);
+  //   else if ((millis() - STRAIGHT.timeStampChangeState) == 2500) relay.digitalWrite(STRAIGHT.relayNum, 1);
+  // }
+  
+  // else if (STRAIGHT.curState == CurrentState::allowed) relay.digitalWrite(STRAIGHT.relayNum, 1);
+
+
+  // if (LEFT.curState == CurrentState::forbidden && STRAIGHT.curState == CurrentState::forbidden && RIGHT.curState == CurrentState::forbidden) 
   // {
   //   relay.digitalWrite(LEFT.relayNum, 1); 
   //   relay.digitalWrite(STRAIGHT.relayNum, 1); 
-  //   relay.digitalWrite(RIGTH.relayNum, 1); 
+  //   relay.digitalWrite(RIGHT.relayNum, 1); 
   // }
 
     
--- src/state.cpp
+++ src/state.cpp
@@ -7,10 +7,15 @@ class Direction {
     public:
         int pinNum, pinMode, relayNum;
         CurrentState curState {CurrentState::forbidden};
+        uint32_t timeStampChangeStateFlashing {0};
         uint32_t timeStampChangeState {0};
+        byte prevPinState {0};
+        bool requestChangeState {false};
+        bool rest {true};
+        bool debounce {true};
 
     public:
-        Direction (int pinNum, int pinMode, int relayNum) : pinNum {pinNum}, pinMode {pinMode}, relayNum {relayNum} 
+        Direction (int pinNum, int pinMode, int relayNum) : pinNum {pinNum}, pinMode {pinMode}, relayNum {relayNum}
             { }
             
         void setState(CurrentState state)
@@ -20,9 +25,26 @@ class Direction {
 
         bool pinIsActive()
         {
-            if (pinMode = INPUT_PULLUP) return !digitalRead(pinNum);
+            if (pinMode == INPUT_PULLUP) return !digitalRead(pinNum);
             return digitalRead(pinNum);
         }
 
+};
 
+
+class TrafficLightT5 {
+    public:
+        Direction* left;
+        Direction* straight;
+        Direction* right;
+        CurrentState curState {CurrentState::forbidden};
+        uint32_t timeStampChangeState {0};
+
+        TrafficLightT5(Direction* left, Direction* straight, Direction* right): left{left}, straight {straight}, right {right}, curState {CurrentState::forbidden }
+        { }
+
+        void setState(CurrentState state)
+        {
+            curState = state;
+        }
 };
